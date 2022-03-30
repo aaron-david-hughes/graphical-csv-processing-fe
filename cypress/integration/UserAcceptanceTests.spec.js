ec2d292a0b738e5fa06f8514b01c2644aa4a24dd
aaronhughes <aaronhughes21@hotmail.co.uk>
2022-03-30 21:22:31 +0100

    cypress alternative test approach

--- cypress/integration/UserAcceptanceTests.spec.js
+++ cypress/integration/UserAcceptanceTests.spec.js
@@ -85,177 +85,9 @@ describe('User Acceptance Tests', () => {
         cy.get('button#delete').click();
     });
 
-    test('process a query from scratch - the list of people and count who scored less than 45', graphId => {
-        addOpenFileNodeAndValidate(formGraphElementId(graphId, 'x'), 'classlist.csv');
-        zoomOut(2);
-        moveNode(formGraphElementId(graphId, 'x'), -180, 0);
-
-        addOpenFileNodeAndValidate(formGraphElementId(graphId, '1l'), 'marks1.csv');
-        zoomOut(2);
-        moveNode(formGraphElementId(graphId, '1l'), -90, -100);
-
-        addOpenFileNodeAndValidate(formGraphElementId(graphId, '2a'), 'marks2.csv');
-        zoomOut(2);
-        moveNode(formGraphElementId(graphId, '2a'), -60, -100);
-
-        addOpenFileNodeAndValidate(formGraphElementId(graphId, '30'), 'marks3.csv');
-        zoomOut(2);
-        moveNode(formGraphElementId(graphId, '30'), -30, -100);
-
-        addOpenFileNodeAndValidate(formGraphElementId(graphId, '3r'), 'marks4.csv');
-        zoomOut(2);
-        moveNode(formGraphElementId(graphId, '3r'), 0, -100);
-
-        addOpenFileNodeAndValidate(formGraphElementId(graphId, '4j'), 'marks5.csv');
-        zoomOut(2);
-        moveNode(formGraphElementId(graphId, '4j'), 30, -100);
-
-        addOpenFileNodeAndValidate(formGraphElementId(graphId, '5c'), 'marks6.csv');
-        zoomOut(2);
-        moveNode(formGraphElementId(graphId, '5c'), 60, -100);
-
-        addUnaryNodeAndConnect(
-            graphId,
-            '66', '5z', '7u', 'a8',
-            -140, 0,
-            'alias', {alias: 'class'}
-        );
-
-        addUnaryNodeAndConnect(
-            graphId,
-            'bj', 'bc', 'df', 'g4',
-            -90, -50,
-            'alias', {alias: 'lab1'}
-        );
-
-        addUnaryNodeAndConnect(
-            graphId,
-            'hk', 'hd', 'jo', 'mo',
-            -60, -50,
-            'alias', {alias: 'lab2'}
-        );
-
-        addUnaryNodeAndConnect(
-            graphId,
-            'o9', 'o2', 'ql', 'tw',
-            -30, -50,
-            'alias', {alias: 'lab3'}
-        );
-
-        addUnaryNodeAndConnect(
-            graphId,
-            'vm', 'vf', 'y6', '11s',
-            0, -50,
-            'alias', {alias: 'lab4'}
-        );
-
-        addUnaryNodeAndConnect(
-            graphId,
-            '13n', '13g', '16f', '1ac',
-            30, -50,
-            'alias', {alias: 'lab5'}
-        );
-
-        addUnaryNodeAndConnect(
-            graphId,
-            '1cc', '1c5', '1fc', '1jk',
-            60, -50,
-            'alias', {alias: 'lab6'}
-        );
-
-        addBinaryNodeAndConnect(
-            graphId, '1lp', 'join_left', {onLeft: 'class.StudentNumber', onRight: 'lab1.StudentNumber'},
-            -90, 0,
-            [
-                {fromId: '1li', toId: '1ox', edgeId: '1tg'},
-                {fromId: '1tu', toId: '1xe', edgeId: '226'}
-            ]
-        );
-
-        addBinaryNodeAndConnect(
-            graphId, '24j', 'join_left', {onLeft: 'class.StudentNumber', onRight: 'lab2.StudentNumber'},
-            -60, 0,
-            [
-                {fromId: '24i', toId: '285', edgeId: '2d8'},
-                {fromId: '2dn', toId: '2hl', edgeId: '2mx'}
-            ]
-        );
-
-        addBinaryNodeAndConnect(
-            graphId, '2pi', 'join_left', {onLeft: 'class.StudentNumber', onRight: 'lab3.StudentNumber'},
-            -30, 0,
-            [
-                {fromId: '2ph', toId: '2ti', edgeId: '2z5'},
-                {fromId: '2zl', toId: '33x', edgeId: '39t'}
-            ]
-        );
-
-        addBinaryNodeAndConnect(
-            graphId, '3cm', 'join_left', {onLeft: 'class.StudentNumber', onRight: 'lab4.StudentNumber'},
-            0, 0,
-            [
-                {fromId: '3cl', toId: '3h0', edgeId: '3n7'},
-                {fromId: '3no', toId: '3se', edgeId: '3yu'}
-            ]
-        );
-
-        addBinaryNodeAndConnect(
-            graphId, '41v', 'join_left', {onLeft: 'class.StudentNumber', onRight: 'lab5.StudentNumber'},
-            30, 0,
-            [
-                {fromId: '41u', toId: '46n', edgeId: '4de'},
-                {fromId: '4dw', toId: '4j0', edgeId: '4ng'}
-            ]
-        );
-
-        addBinaryNodeAndConnect(
-            graphId, '4ta', 'join_left', {onLeft: 'class.StudentNumber', onRight: 'lab6.StudentNumber'},
-            60, 0,
-            [
-                {fromId: '4t9', toId: '4yg', edgeId: '55r'},
-                {fromId: '56a', toId: '5bs', edgeId: '5jc'}
-            ]
-        );
-
-        addUnaryNodeAndConnect(
-            graphId,
-            '5mt', '5ms', '5sd', '608',
-            60, 50,
-            'take_columns',
-            {columns: 'class.StudentNumber,Practical1,Practical2,Practical3,Practical4,Practical5,Practical6'}
-        );
-
-        addUnaryNodeAndConnect(
-            graphId,
-            '63u', '63t', '69m', '6hs',
-            60, 100,
-            'drop_alias',
-            {}
-        );
-
-        addUnaryNodeAndConnect(
-            graphId,
-            '6lj', '6li', '6rj', '700',
-            30, 100,
-            'row_average',
-            {columns: 'Practical1,Practical2,Practical3,Practical4,Practical5,Practical6', newName: 'Average'}
-        );
-
-        addUnaryNodeAndConnect(
-            graphId,
-            '73w', '73v', '7a4', '7iw',
-            0, 100,
-            'less_than_filter',
-            {column: 'Average', condition: '45'}
-        );
-
-        addUnaryNodeAndConnect(
-            graphId,
-            '7mx', '7mw', '7td', '82g',
-            -30, 100,
-            'write_file',
-            {name: 'AverageScores.csv'}
-        );
+    test('load graph - the list of people and count who scored less than 45', () => {
+        cy.get('button#loadButton').click();
+        cy.get('input#loadGraphFileInput').attachFile('ScoreLessThan45.json');
 
         const downloadsFolder = Cypress.config('downloadsFolder')
         const downloadedFilename = path.join(downloadsFolder, 'ScoreLessThan45Result.zip')
@@ -267,13 +99,11 @@ describe('User Acceptance Tests', () => {
 
         verifyFile(downloadedFilename, ScoreLessThan45ExpectedData);
     });
-
-    // test('save a query and reload to process');
 });
 
 function test(name, callback) {
     it(`${name}`, {
-        defaultCommandTimeout: 30000
+        defaultCommandTimeout: 10000
     }, () => {
         getGraphIdContext(callback);
     });
@@ -287,24 +117,6 @@ function getGraphIdContext(callback) {
 
 function formGraphElementId(graphId, elementId) {
     return `path#ac_path_${graphId}_${elementId}`;
-}
-
-function addUnaryNodeAndConnect(graphId, addNodeId, fromId, toId, edgeId, x, y, nodeType, keyValuePairs) {
-    addNodeAndValidate(formGraphElementId(graphId, addNodeId), nodeType, keyValuePairs);
-    zoomOut(2);
-    moveNode(formGraphElementId(graphId, addNodeId), x, y);
-    addEdgeAndValidate(
-        formGraphElementId(graphId, edgeId),
-        formGraphElementId(graphId, fromId),
-        formGraphElementId(graphId, toId)
-    );
-}
-
-function addBinaryNodeAndConnect(graphId, addNodeId, nodeType, keyValuePairs, x, y, edges) {
-    addNodeAndValidate(formGraphElementId(graphId, addNodeId), nodeType, keyValuePairs);
-    zoomOut(2);
-    moveNode(formGraphElementId(graphId, addNodeId), x, y);
-    addEdgesAndValidate(graphId, edges);
 }
 
 function addNodeAndValidate(id, type, keyValuePairs) {
@@ -330,14 +142,6 @@ function addNodeAndValidate(id, type, keyValuePairs) {
     checkPresence(id);
 }
 
-function addOpenFileNodeAndValidate(id, filepath) {
-    cy.get('select#nodeOptions').select('open_file');
-    cy.get('button#next').click();
-    cy.get('input#fileInput').attachFile(filepath);
-    cy.get('button#addNode').click();
-    checkPresence(id);
-}
-
 function addEdgeAndValidate(edge, from, to) {
     cy.get(from).click({ force: true });
     cy.wait(1000);
@@ -345,18 +149,6 @@ function addEdgeAndValidate(edge, from, to) {
     cy.get(to).click({ force: true });
 
     checkPresence(edge);
-}
-
-function addEdgesAndValidate(graphId, edges) {
-    addEdgeAndValidate(
-        formGraphElementId(graphId, edges[0].edgeId),
-        formGraphElementId(graphId, edges[0].fromId),
-        formGraphElementId(graphId, edges[0].toId)
-    );
-    cy.get(formGraphElementId(graphId, edges[1].fromId)).click();
-    cy.get(formGraphElementId(graphId, edges[1].toId)).click();
-
-    checkPresence(formGraphElementId(graphId, edges[1].edgeId));
 }
 
 function verifyFormContent(keyValuePairs) {
@@ -385,12 +177,6 @@ function checkMove(id, newX, newY) {
         expect(dataParts[4]).to.deep.equal(newX);
         expect(dataParts[5]).to.deep.equal(newY);
     });
-}
-
-function zoomOut(number) {
-    for (let i = 0; i < number; i ++) {
-        cy.get('.anychart-zoom-zoomOut').click();
-    }
 }
 
 function payloadValidation(expectedData, actualData) {
